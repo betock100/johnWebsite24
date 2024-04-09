a3fdc83b9c26a14b1043ba46d714176cbcecdbac
Alberto Tuesta <betock100@yahoo.com>
2024-04-09 15:37:50 -0400

    from html to php

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JRC Electric</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap">
+</head>
+<body>
+    <div class="container">
+        <?php include 'navbar.php'; ?> 
+    </div>
+    
+    <script src="script.js"></script>
+
+</body>
+</html>
---
+++ navBar.php
@@ -0,0 +1,27 @@
+<div class="navBar">
+    <div class="logo"><!-- logo --></div>
+    <!--nnnnnnaaavvv barrrrr hamburgerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr-->
+    <label for="toggle-menu" class="hamburger-icon">&#9776;</label>
+    <button class="close-button" aria-label="Close">X</button>
+
+    <nav class="navbar2">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+
+    <!--nnnnnnaaavvv barrrrr hamburgerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr-->
+    
+    <div class="navLinks">
+        <a href="index.html">Home</a>
+        <a href="#">About us</a>
+        <a href="#">Projects</a>
+        <a href="#">Services</a>
+        <a href="contact.php" class="contactButton">Contact us</a>
+    </div>                  
+
+</div>
